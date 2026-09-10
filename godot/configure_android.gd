efa387ee087c406da3ca2b4ee0e8da936b3f09b7
Haselton <147449208+Haselton@users.noreply.github.com>
2026-09-10 05:52:37 -0700

    Run Android editor configuration as headless SceneTree

--- godot/configure_android.gd
+++ godot/configure_android.gd
@@ -1,10 +1,10 @@
-@tool
-extends EditorScript
+extends SceneTree
 
-func _run():
-    var settings = get_editor_interface().get_editor_settings()
+func _initialize():
+    var settings = EditorInterface.get_editor_settings()
     settings.set_setting("export/android/android_sdk_path", OS.get_environment("ANDROID_HOME"))
     settings.set_setting("export/android/java_sdk_path", OS.get_environment("JAVA_HOME"))
     settings.save()
     print("Android SDK configured: ", settings.get_setting("export/android/android_sdk_path"))
     print("Java SDK configured: ", settings.get_setting("export/android/java_sdk_path"))
+    quit()
